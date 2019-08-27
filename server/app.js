e1af3694a2b277e5da91aa1a5c5013bbf90ec7c1
Rey Mercado <reymercado.usa@gmail.com>
2019-08-27 15:38:46 -0700

    finally connected api.

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const db = require('../db/index.js');
 
 const app = express();
-app.use('/restaurants/:rid', express.static(path.resolve(__dirname, '../public')));
-console.log('got here');
-app.get('/api/restaurants/:rid/menus', (req, res) => {
+// app.use('/restaurants/:rid', express.static(path.resolve(__dirname, '../public')));
+// console.log('got here');
+app.get('/api/restaurants/:rid', (req, res) => {
     db.getMenus(req.params.rid, (err, docs) => {
         if (err) {
             console.log(err);
@@ -14,5 +14,10 @@ app.get('/api/restaurants/:rid/menus', (req, res) => {
         res.status(200).send(docs);
     });
 });
+
+app.get('/', (req, res) => {
+  res.send({'hello': 'world'})
+  // res.send(db[0]);
+})
 
 module.exports = app;
